dde7740511ed70b5917cc5c686b6eaae10b89e66
Simon Gray <simongray@gmail.com>
2017-10-04 23:02:05 +0800

    now can produce dependency graphs using Clojure code

--- src/corenlp_clj/core.clj
+++ src/corenlp_clj/core.clj
@@ -1,12 +1,49 @@
 (ns corenlp-clj.core
   (:import [java.util Properties]
-           [edu.stanford.nlp.pipeline StanfordCoreNLP]))
+           [edu.stanford.nlp.pipeline StanfordCoreNLP Annotation]
+           [edu.stanford.nlp.ling CoreAnnotations$SentencesAnnotation
+                                  CoreAnnotations$TokensAnnotation]
+           [edu.stanford.nlp.semgraph SemanticGraphCoreAnnotations$BasicDependenciesAnnotation
+                                      SemanticGraphCoreAnnotations$EnhancedDependenciesAnnotation
+                                      SemanticGraphCoreAnnotations$EnhancedPlusPlusDependenciesAnnotation]))
 
+(def dep-types {:basic SemanticGraphCoreAnnotations$BasicDependenciesAnnotation
+                :enhanced SemanticGraphCoreAnnotations$EnhancedDependenciesAnnotation
+                :enhanced++ SemanticGraphCoreAnnotations$EnhancedPlusPlusDependenciesAnnotation})
 
-(defn properties [& args]
+(defn properties
+  [& args]
   (let [props (Properties.)]
     (.putAll props (apply hash-map args))
     props))
 
-(defn pipeline [props]
-  (StanfordCoreNLP. ^Properties props))
+(defn pipeline
+  [^Properties props]
+  (StanfordCoreNLP. props))
+
+(defn annotation
+  [^StanfordCoreNLP pipeline ^String s]
+  (let [annotation (Annotation. s)]
+    (.annotate pipeline annotation) ; mutating!
+    annotation))
+
+(defn sentences
+  [^Annotation annotation]
+  (.get annotation CoreAnnotations$SentencesAnnotation))
+
+(defn tokens
+  [^Annotation annotation]
+  (.get annotation CoreAnnotations$TokensAnnotation))
+
+(defn dependencies
+  ([^Annotation annotation type]
+   (.get annotation (dep-types type)))
+  ([^Annotation annotation]
+   (dependencies annotation :enhanced++)))
+
+(defn dep
+  "Shorthand version of dependencies function that operates directly on a String s."
+  ([^StanfordCoreNLP pipeline ^String s type]
+   (map #(dependencies % type) (sentences (annotation pipeline s))))
+  ([^StanfordCoreNLP pipeline ^String s]
+   (dep pipeline s :enhanced++)))
--- src/corenlp_clj/example.clj
+++ src/corenlp_clj/example.clj
@@ -3,3 +3,11 @@
 
 ;; how to set up the dependency parse annotator
 (def nlp (pipeline (properties "annotators" "tokenize, ssplit, pos, depparse")))
+
+(def example "This is just an example sentence in English. This is another.")
+
+;; the long way of getting a dependency graph
+(def deps-long (map dependencies (sentences (annotation nlp example))))
+
+;; the short way of getting a dependency graph
+(def deps-short (dep nlp example))
