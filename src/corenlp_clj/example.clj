c128a55b2bde4f5e441b2a3ad973fc0e41fdb134
Simon Gray <simongray@gmail.com>
2017-10-09 17:43:35 +0800

    more succinct examples

--- src/corenlp_clj/example.clj
+++ src/corenlp_clj/example.clj
@@ -6,36 +6,20 @@
 (def nlp (pipeline {"annotators" (prerequisites ["depparse" "lemma" "ner"])}))
 
 ;; parts of speech
-(-> "This is an example sentence. That is another."
-    nlp
-    sentences
-    tokens
-    pos)
+(-> "This is an example sentence. That is another." nlp sentences tokens pos)
 ;=> (("DT" "VBZ" "DT" "NN" "NN" ".") ("DT" "VBZ" "DT" "."))
 
 ;; named entity tags
-(-> "Anna went travelling in China."
-    nlp
-    sentences
-    tokens
-    ner)
+(-> "Anna went travelling in China." nlp sentences tokens ner)
 ;=> (("PERSON" "O" "O" "O" "LOCATION" "O"))
 
 ;; lemmatisation
-(-> "She has beaten him before."
-    nlp
-    sentences
-    tokens
-    lemma)
+(-> "She has beaten him before." nlp sentences tokens lemma)
 ;=> (("she" "have" "beat" "he" "before" "."))
 
 ;; dependencies (according to dependency grammar)
-(-> "A sentence has dependencies."
-    nlp
-    sentences
-    dependencies)
-;=>
-;(#object[edu.stanford.nlp.semgraph.SemanticGraph
+(-> "A sentence has dependencies." nlp sentences dependencies)
+;=> (#object[edu.stanford.nlp.semgraph.SemanticGraph])
 ;         0x3ed8576b
 ;         "-> has/VBZ (root)
 ;            -> sentence/NN (nsubj)
