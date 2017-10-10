8277f95606f06df41ec6a1b54833fa3f5e999649
Simon Gray <simongray@gmail.com>
2017-10-10 13:36:27 +0800

    added Chinese models as separate dependency (will probably remove models and models-chinese deps at some point)

--- project.clj
+++ project.clj
@@ -6,4 +6,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
                  [edu.stanford.nlp/stanford-corenlp "3.8.0"]
-                 [edu.stanford.nlp/stanford-corenlp "3.8.0" :classifier "models"]])
+                 [edu.stanford.nlp/stanford-corenlp "3.8.0" :classifier "models"]
+                 [edu.stanford.nlp/stanford-corenlp "3.8.0" :classifier "models-chinese"]])
