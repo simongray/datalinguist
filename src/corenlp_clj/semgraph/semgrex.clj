95b4277e303034b716068d2820cfa390f71677a0
Simon Gray <simongray@gmail.com>
2017-10-17 21:23:31 +0800

    renamed sgx to se

--- src/corenlp_clj/semgraph/semgrex.clj
+++ src/corenlp_clj/semgraph/semgrex.clj
@@ -3,51 +3,51 @@
            [edu.stanford.nlp.semgraph SemanticGraph]))
 
 ;;;; This namespace contains implementations of functions relating to semgrex in Stanford CoreNLP.
-;;;; sgx-pattern, sgx-matcher, sgx-find, and sgx-seq mimic standard Clojure regex functions.
+;;;; se-pattern, se-matcher, se-find, and se-seq mimic standard Clojure regex functions.
 ;;;; Matches are of type IndexedWord. Underlying data can be obtained using the functions in corenlp-clj.annotations.
 
-(defn sgx-pattern
-  "Returns an instance of SemgrexPattern, for use, e.g. in sg-matcher."
+(defn se-pattern
+  "Returns an instance of SemgrexPattern, for use, e.g. in se-matcher."
   [^String s]
   (SemgrexPattern/compile s))
 
-(defn sgx-matcher
-  "Returns an instance of SemgrexMatcher, for use, e.g. in sg-find."
+(defn se-matcher
+  "Returns an instance of SemgrexMatcher, for use, e.g. in se-find."
   [^SemgrexPattern p ^SemanticGraph g]
   (.matcher p g))
 
-(defn sgx-find
+(defn se-find
   "Returns the next semgrex match, if any, of string to pattern, using SemgrexMatcher.findNextMatchingNode()."
   [^SemgrexMatcher m]
   (when (.findNextMatchingNode m) (.getMatch m)))
 
-(defn sgx-seq
+(defn se-seq
   "Returns a list of all unique matches of SemgrexPattern in SemanticGraph (note: not lazy)."
-  [^SemgrexPattern sgx ^SemanticGraph g]
-  (let [m (sgx-matcher sgx g)]
+  [^SemgrexPattern p ^SemanticGraph g]
+  (let [m (se-matcher p g)]
     (loop [matches ()]
       (if (.findNextMatchingNode m)
         (recur (conj matches (.getMatch m)))
         matches))))
 
-(defn sgx-node-names
-  "Returns list of matching labeled node names after latest call to sgx-find."
+(defn se-node-names
+  "Returns list of matching labeled node names after latest call to se-find."
   [^SemgrexMatcher m]
   (.getNodeNames m))
 
-(defn sgx-relation-names
-  "Returns list of matching labeled relation names after latest call to sgx-find."
+(defn se-relation-names
+  "Returns list of matching labeled relation names after latest call to se-find."
   [^SemgrexMatcher m]
   (.getRelationNames m))
 
-(defn sgx-nodes
-  "Returns a map of matching node names to nodes after latest call to sgx-find."
+(defn se-nodes
+  "Returns a map of matching node names to nodes after latest call to se-find."
   [^SemgrexMatcher m]
   (into {} (map #(vector (keyword %) (.getNode m %))
-                (sgx-node-names m))))
+                (se-node-names m))))
 
-(defn sgx-relations
-  "Returns a map of matching relation names to relations after latest call to sgx-find."
+(defn se-relations
+  "Returns a map of matching relation names to relations after latest call to se-find."
   [^SemgrexMatcher m]
   (into {} (map #(vector (keyword %) (.getRelnString m %))
-                (sgx-relation-names m))))
+                (se-relation-names m))))
