89a9425225301485b08df3a49715b6b55586e270
Simon Gray <simongray@gmail.com>
2018-02-21 15:56:18 +0100

    upgraded to Java 9, Clojure 1.9

--- project.clj
+++ project.clj
@@ -1,10 +1,8 @@
 (defproject computerese "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :jvm-opts ["-Xmx2G"] ; CoreNLP uses up memory fast
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
+  :description "Stanford CoreNLP in idiomatic Clojure."
+  :url "https://github.com/simongray/computerese"
+  :jvm-opts ["-Xmx2G"]                                      ; CoreNLP uses up memory fast
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [aysylu/loom "1.0.0"]
                  [edu.stanford.nlp/stanford-corenlp "3.8.0"]]
 
