e51bd48445708538463713d852416d55483c91b1
Simon Gray <simongray@gmail.com>
2018-12-08 19:36:21 +0100

    updated dependencies

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/simongray/computerese"
   :jvm-opts ["-Xmx2G"]                                      ; CoreNLP uses up memory fast
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [aysylu/loom "1.0.0"]
-                 [edu.stanford.nlp/stanford-corenlp "3.8.0"]]
+                 [aysylu/loom "1.0.2"]
+                 [edu.stanford.nlp/stanford-corenlp "3.9.2"]]
 
-  :profiles {:dev {:dependencies [[edu.stanford.nlp/stanford-corenlp "3.8.0" :classifier "models"]
-                                  [edu.stanford.nlp/stanford-corenlp "3.8.0" :classifier "models-chinese"]]}})
+  :profiles {:dev {:dependencies [[edu.stanford.nlp/stanford-corenlp "3.9.2" :classifier "models"]
+                                  [edu.stanford.nlp/stanford-corenlp "3.9.2" :classifier "models-chinese"]]}})
