22cba115c38197c672a7d6fcc3f0e3bc3dc40990
Simon Gray <simongray@gmail.com>
2018-12-16 00:32:24 +0100

    clean up semgrex ns

--- src/computerese/semgraph/semgrex.clj
+++ src/computerese/semgraph/semgrex.clj
@@ -1,31 +1,32 @@
-(ns computerese.semgraph.semgrex
-  (:import [edu.stanford.nlp.semgraph.semgrex SemgrexPattern SemgrexMatcher]
-           [edu.stanford.nlp.semgraph SemanticGraph]))
+(ns ^{:doc "Fns dealing with semgrex in CoreNLP"} computerese.semgraph.semgrex
+  (:import [edu.stanford.nlp.semgraph SemanticGraph]
+           [edu.stanford.nlp.semgraph.semgrex SemgrexPattern
+                                              SemgrexMatcher]))
 
-;;;; This namespace contains implementations of functions relating to semgrex in Stanford CoreNLP.
-;;;;
-;;;; Se-pattern, se-matcher, se-find, and se-seq mimic standard Clojure regex functions.
-;;;; The other functions prepended with "se-" deal with extracting named nodes and relations from matches.
-;;;;
-;;;; Matches are of type IndexedWord. Underlying data can be obtained using the functions in corenlp-clj.annotations.
+;; Se-pattern, se-matcher, se-find, and se-seq mimic Clojure regex functions.
+;; The other functions prepended with "se-" deal with extracting named nodes and
+;; relations from matches. Matches are of type IndexedWord. Underlying data can
+;; be obtained using the functions in `computere.annotations`.
 
 (defn se-pattern
-  "Returns an instance of SemgrexPattern, for use, e.g. in se-matcher."
+  "Return an instance of SemgrexPattern, for use, e.g. in se-matcher."
   [^String s]
   (SemgrexPattern/compile s))
 
 (defn se-matcher
-  "Returns an instance of SemgrexMatcher, for use, e.g. in se-find."
+  "Return an instance of SemgrexMatcher, for use, e.g. in se-find."
   [^SemgrexPattern p ^SemanticGraph g]
   (.matcher p g))
 
 (defn se-find
-  "Returns the next semgrex match, if any, of string to pattern, using SemgrexMatcher.findNextMatchingNode()."
+  "Return the next semgrex match, if any, of string to pattern, using
+  SemgrexMatcher.findNextMatchingNode()."
   [^SemgrexMatcher m]
   (when (.findNextMatchingNode m) (.getMatch m)))
 
 (defn se-seq
-  "Returns a list of all unique matches of SemgrexPattern in SemanticGraph (note: not lazy)."
+  "Return a list of all unique matches of SemgrexPattern in SemanticGraph
+  (note: not lazy)."
   [^SemgrexPattern p ^SemanticGraph g]
   (let [^SemgrexMatcher m (se-matcher p g)]
     (loop [matches ()]
@@ -34,35 +35,38 @@
         matches))))
 
 (defn se-node-names
-  "Returns list of matching labeled node names after latest call to se-find."
+  "Return list of matching labeled node names after latest call to se-find."
   [^SemgrexMatcher m]
   (.getNodeNames m))
 
 (defn se-relation-names
-  "Returns list of matching labeled relation names after latest call to se-find."
+  "Return list of matching labeled relation names after latest call to se-find."
   [^SemgrexMatcher m]
   (.getRelationNames m))
 
 (defn se-nodes
-  "Returns a map of matching node names to nodes after latest call to se-find."
+  "Return a map of matching node names to nodes after latest call to se-find."
   [^SemgrexMatcher m]
   (into {} (map #(vector (keyword %) (.getNode m %))
                 (se-node-names m))))
 
 (defn se-relations
-  "Returns a map of matching relation names to relations after latest call to se-find."
+  "Return a map of matching relation names to relations after latest call to
+  se-find."
   [^SemgrexMatcher m]
   (into {} (map #(vector (keyword %) (.getRelnString m %))
                 (se-relation-names m))))
 
 (defn se-find-full
-  "Returns a vector of the next semgrex match, matching named nodes, and matching named relations, if any."
+  "Return a vector of the next semgrex match, matching named nodes, and matching
+  named relations, if any."
   [^SemgrexMatcher m]
   (when-let [match (se-find m)]
     [match (se-nodes m) (se-relations m)]))
 
 (defn se-seq-full
-  "Returns vectors of semgrex match, matching named nodes, and matching named relations for all matches."
+  "Return vectors of semgrex match, matching named nodes, and matching named
+  relations for all matches."
   [^SemgrexPattern p ^SemanticGraph g]
   (let [^SemgrexMatcher m (se-matcher p g)]
     (loop [matches ()]
